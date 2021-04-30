1cd61aa374a5c66d825114838227b50965bb88ef
Facu Mediotte <facumediotte@gmail.com>
2021-04-30 00:43:20 -0300

    Lab01

--- Clase01/JPA-Tutorial/src/main/java/educacionit/jpa/JpaApplication.java
+++ Clase01/JPA-Tutorial/src/main/java/educacionit/jpa/JpaApplication.java
@@ -1,5 +1,7 @@
 package educacionit.jpa;
 
+import educacionit.jpa.dao.ProductoDAO;
+import educacionit.jpa.entidades.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,27 +11,46 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import educacionit.jpa.dao.PersonaDAO;
 import educacionit.jpa.entidades.Persona;
 
+import java.util.Date;
+
 @SpringBootApplication
 //@EntityScan( basePackages = {"educacionit.jpa"} )
 public class JpaApplication implements CommandLineRunner {
 
-    @Autowired
     PersonaDAO personaDAO;
+    ProductoDAO productoDAO;
+
+    @Autowired
+    public JpaApplication(PersonaDAO personaDAO, ProductoDAO productoDAO) {
+        this.personaDAO = personaDAO;
+        this.productoDAO = productoDAO;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         Persona persona = new Persona();
-        persona.setApellido("Perez");
-        persona.setEdad(60);
-        persona.setNombre("Juan");
+        persona.setNombre("Facu");
+        persona.setApellido("Mediotte");
+        persona.setEdad(25);
+
+        //El save funciona como un saveOrUpdate
         personaDAO.save(persona);
 
-        personaDAO.findAll().forEach((p) -> System.out.println(p.getApellido()));
-    }
+        personaDAO.findAll().forEach((p) -> System.out.println(p.getNombre()));
 
+        //Producto Lab01
+        Producto p = new Producto();
+        p.setDescripcion("PS4");
+        p.setPrecio(80000.0);
+        p.setFechaAlta(new Date());
+
+        productoDAO.save(p);
+
+        productoDAO.findAll().forEach(System.out::println);
+    }
 
 }
--- Clase01/JPA-Tutorial/src/main/java/educacionit/jpa/dao/ProductoDAO.java
+++ Clase01/JPA-Tutorial/src/main/java/educacionit/jpa/dao/ProductoDAO.java
@@ -1,11 +1,10 @@
 package educacionit.jpa.dao;
 
+import educacionit.jpa.entidades.Producto;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import educacionit.jpa.entidades.Persona;
-
 @Repository
-public interface PersonaDAO extends CrudRepository<Persona, Integer> {
+public interface ProductoDAO extends CrudRepository<Producto, Integer> {
 
 }
